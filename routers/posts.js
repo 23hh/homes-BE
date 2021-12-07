725ade026ead4e1300850efd031383c434bf95b0
23h <lsh6166@gmail.com>
2021-12-07 15:04:34 +0900

    싱협 : models 변수명 변경

--- routers/posts.js
+++ routers/posts.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Board = require("../models/posts"); // 스키마에서 모델을 가져옴
+const Posts = require("../models/posts"); // 스키마에서 모델을 가져옴
 const router = express.Router();
 
 module.exports = router;
